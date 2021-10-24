8e8f190108733c545ce611d8b2aa45c5b2e0fb31
nickguerra <nickg@maliciousgames.com>
2021-10-24 07:53:52 -0500

    Added process method to get image path for employee photos

--- RazorPagesDemo/Pages/Employees/Edit.cshtml.cs
+++ RazorPagesDemo/Pages/Employees/Edit.cshtml.cs
@@ -1,18 +1,23 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPagesDemo.Models;
 using RazorPagesDemo.Services;
+using System;
+using System.IO;
 
 namespace RazorPagesDemo.Pages.Employees
 {
     public class EditModel : PageModel
     {
         private readonly IEmployeeRepository _employeeRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public EditModel(IEmployeeRepository employeeRepository)
+        public EditModel(IEmployeeRepository employeeRepository, IWebHostEnvironment webHostEnvironment)
         {
             _employeeRepository = employeeRepository;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public Employee Employee { get; set; }
@@ -34,8 +39,31 @@ namespace RazorPagesDemo.Pages.Employees
 
         public IActionResult OnPost(Employee employee)
         {
+            if(Photo != null)
+            {
+                employee.PhotoPath = ProcessUploadedFile();
+            }
+
            Employee = _employeeRepository.Update(employee);
             return RedirectToPage("Index");
+        }
+
+        private string ProcessUploadedFile()
+        {
+            string uniqueFileName = null;
+
+            if (Photo != null)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + Photo.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    Photo.CopyTo(fileStream);
+                }
+            }
+
+            return uniqueFileName;
         }
 
     }
